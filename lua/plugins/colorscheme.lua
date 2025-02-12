5a235e72f18adef9c68673ca83b301976b000ac3
Lehrer, Brian <661570+blehrer@users.noreply.github.com>
2025-02-12 15:02:39 -0800

    install some external plugins

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  { "AlexvZyl/nordic.nvim" },
+  { "neanias/everforest-nvim" },
+  { "rebelot/kanagawa.nvim" },
+  { "catppuccin/nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "kanagawa",
+    },
+  },
+}
